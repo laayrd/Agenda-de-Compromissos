69154732041d553f9007ce1dfb442c4d48b4fd59
Layssa Rodrigues <layssa21.alves@gmail.com>
2025-05-07 19:01:59 -0300

    Implementação da Classe Usuário

---
+++ AgendaDeCompromisso/Modelos/Usuario.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace AgendaDeCompromisso.Modelos;
+
+public class Usuario
+{
+    public string NomeUsuario { get; }
+    private readonly List<Compromisso> _compromissos = new();
+    public IReadOnlyCollection<Compromisso> Compromissos => _compromissos;
+
+    public Usuario (string nomeUsuario) {
+        if (string.IsNullOrWhiteSpace(nomeUsuario))
+            throw new ArgumentException("Nome de usuário é obrigatório");
+        NomeUsuario = nomeUsuario;
+    }
+
+    public void AdicionarCompromisso (Compromisso compromisso) {
+        if(compromisso == null) {
+            throw new ArgumentNullException();
+        }
+        _compromissos.Add(compromisso);
+    }
+}
---
+++ AgendaDeCompromisso/Program.cs
@@ -0,0 +1,13 @@
+﻿using AgendaDeCompromisso.Modelos;
+
+Console.Write("Digite o nome do usuário: ");
+string nome = Console.ReadLine();
+
+try {
+    Usuario usuario = new(nome);
+
+    Console.WriteLine("\nUsuário criado com sucesso");
+    Console.WriteLine($"Nome: {usuario.NomeUsuario}");
+} catch (Exception e) {
+    Console.WriteLine($"\nErro ao criar usuário: {e.Message}");
+}
