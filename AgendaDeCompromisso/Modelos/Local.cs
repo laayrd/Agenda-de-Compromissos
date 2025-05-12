6076823fdc53b886b4028382ef75cdb9911239a3
Layssa Rodrigues <layssa21.alves@gmail.com>
2025-05-12 20:24:40 -0300

    Ajustes na Classe Local

--- AgendaDeCompromisso/Modelos/Local.cs
+++ AgendaDeCompromisso/Modelos/Local.cs
@@ -4,22 +4,50 @@ namespace AgendaDeCompromisso.Modelos;
 
 public class Local
 {
-    public string Nome { get; }
-    public string Endereco { get; }
+    private string _nome;
+    private string _endereco;
+    private int _capacidadeMaxima;
 
-    public Local(string nome, string endereco)
-    {
-        if (string.IsNullOrWhiteSpace(nome))
-            throw new ArgumentException("Nome do local é obrigatório");
-        if (string.IsNullOrWhiteSpace(endereco))
-            throw new ArgumentException("Endereço do local é obrigatório");
+    public string Nome { get {
+        return _nome;
+    } }
+    public string Endereco { get {
+        return _endereco;
+    } }
+    public int CapacidadeMaxima {get {
+        return _capacidadeMaxima;
+    } }
+    public readonly List<string> ErrosDeValidacao = [];
+    public Local(string nome, string endereco, int Capacidade) {
+        _nome = nome;
+        _endereco = endereco;
+        _capacidadeMaxima = Capacidade;
+        if(!ValidarLocal()) {
+            throw new ArgumentException(string.Join("\n", ErrosDeValidacao));
+        }
+    }
 
-        Nome = nome;
-        Endereco = endereco;
+    public void ValidarCapacidade(int quantidade) {
+        if (quantidade > _capacidadeMaxima) {
+            throw new InvalidOperationException("A quantidade de participantes excede a capacidade do local.");
+        }
+    }
+
+    public bool ValidarLocal() {
+        if (string.IsNullOrWhiteSpace(_nome)) {
+            ErrosDeValidacao.Add("Nome do local é obrigatório");
+        }       
+        if (string.IsNullOrWhiteSpace(_endereco)) {
+            ErrosDeValidacao.Add("Endereço do local é obrigatório");
+        }
+        if (_capacidadeMaxima <= 0) {
+            ErrosDeValidacao.Add("Capacidade máxima deve ser maior que zero.");
+        }
+        return ErrosDeValidacao.Count == 0;
     }
 
     public override string ToString()
     {
-        return $"{Nome} ({Endereco})";
+        return $"{_nome}\nEndereço: {_endereco}\nCapacidade Máxima: {_capacidadeMaxima}";
     }
 }
